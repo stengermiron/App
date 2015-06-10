18f255a443124160877dd99c276adb7e86e5ccf5
Phil Stenger <pstenger@nd.edu>
2015-06-09 23:47:00 -0400

    Lifecycle

--- app/src/main/java/com/example/phillip/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/phillip/myapplication2/MainActivity.java
@@ -7,11 +7,42 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private final String TAG = "TKT;"
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+    }
+    @Override
+    protected void onStart(Bundle savedInstanceState) {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 
     @Override
